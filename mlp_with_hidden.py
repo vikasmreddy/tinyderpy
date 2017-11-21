e0695215b9ba332995ebf8791e09af1e9c4e8d77
Vikas Reddy <vikas@occipital.com>
2017-11-21 00:07:25 -0700

    Reducing number of hidden layers

--- mlp_with_hidden.py
+++ mlp_with_hidden.py
@@ -1,20 +1,20 @@
 import tensorflow as tf
 
 # Network Parameters
-n_hidden_1 = 9
-n_hidden_2 = 9
 n_input = 19
+n_hidden_1 = 3
+#n_hidden_2 = 9
 n_classes = 9
 
 # Store layers weight & bias
 weights = {
     'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
-    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
-    'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]))
+    #'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_hidden_1, n_classes]))
 }
 biases = {
     'b1': tf.Variable(tf.random_normal([n_hidden_1])),
-    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+    #'b2': tf.Variable(tf.random_normal([n_hidden_2])),
     'out': tf.Variable(tf.random_normal([n_classes]))
 }
 
@@ -22,7 +22,7 @@ def create_mlp(x):
     # Hidden fully connected layer with 9 neurons
     layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
     # Hidden fully connected layer with 9 neurons
-    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    #layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
     # Output fully connected layer with a neuron for each class
-    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    out_layer = tf.matmul(layer_1, weights['out']) + biases['out']
     return out_layer
